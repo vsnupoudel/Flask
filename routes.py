c9aa7fec79e68ecf612e57066b47191ec8a74340
vsnupoudel <replytobishnu@gmail.com>
2021-06-27 11:51:17 +0000

    Add files via upload

---
+++ routes.py
@@ -0,0 +1,13 @@
+from app import app
+from flask import render_template
+import forms
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('./index.html')
+
+@app.route('/about', method=['GET','POST'])
+def about():
+    form = forms.AddTaskForm()
+    return render_template('./about.html', form= form)
