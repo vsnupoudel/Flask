737c897d56a00f49bfeaa534f66a1bf5110a4cac
vsnupoudel <replytobishnu@gmail.com>
2021-06-27 12:02:34 +0000

    Add files via upload

--- routes.py
+++ routes.py
@@ -7,7 +7,11 @@ import forms
 def index():
     return render_template('./index.html')
 
-@app.route('/about', method=['GET','POST'])
+@app.route('/about', methods=['GET','POST'])
 def about():
     form = forms.AddTaskForm()
+    if form.validate_on_submit():
+        print('Print title data', form.title.data)
+        return render_template('about.html', form= form,
+        title= form.title.data)
     return render_template('./about.html', form= form)
