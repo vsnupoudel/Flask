267db27542a4910ace65abc695b318a8fbebe06c
vsnupoudel <replytobishnu@gmail.com>
2021-06-27 16:57:04 +0000

    Add files via upload

--- routes.py
+++ routes.py
@@ -1,17 +1,24 @@
-from app import app
-from flask import render_template
+from app import app, db
+from flask import render_template, redirect, url_for
+from models import Task
+from datetime import datetime
 import forms
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('./index.html')
+    tasks = Task.query.all()
+    return render_template('index.html', tasks=tasks)
 
-@app.route('/about', methods=['GET','POST'])
-def about():
+@app.route('/add', methods=['GET','POST'])
+def add():
     form = forms.AddTaskForm()
     if form.validate_on_submit():
-        print('Print title data', form.title.data)
-        return render_template('about.html', form= form,
-        title= form.title.data)
-    return render_template('./about.html', form= form)
+        t = Task(title=form.title.data, date=datetime.utcnow())
+        db.session.add(t)
+        db.session.commit()
+        return redirect(url_for('index'))
+       #return render_template('about.html', form= form,
+       # title= form.title.data)
+
+    return render_template('./add.html', form= form)
