bfc1c5d8e5bb96e5bb6973aa5893b011d405a5ea
vsnupoudel <replytobishnu@gmail.com>
2021-06-28 06:38:49 +0000

    adding delete functionality

--- forms.py
+++ forms.py
@@ -5,3 +5,6 @@ from wtforms.validators import DataRequired
 class AddTaskForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired() ] )
     submit = SubmitField('Submit')
+
+class DeleteTaskForm(FlaskForm):
+    submit = SubmitField('Delete')
--- routes.py
+++ routes.py
@@ -40,4 +40,27 @@ def edit(task_id):
         form.title.data = task.title
         return render_template('edit.html', form= form, 
         task_id=task_id)
+    else:
+        flash('Task not found')
     return redirect(url_for('index'))
+
+
+
+@app.route('/delete/<int:task_id>', methods =['GET','POST'])
+def delete(task_id):
+    task = Task.query.get(task_id)
+    form = forms.DeleteTaskForm()
+    if task:
+        if form.validate_on_submit():
+            db.session.delete(task)
+            db.session.commit()
+            flash('Task has been updated')
+            return redirect(url_for('index'))
+        
+        return render_template('delete.html', form= form, 
+        task_id=task_id, title=task.title)
+    else:
+        flash('Task not found')
+    return redirect(url_for('index'))
+
+
