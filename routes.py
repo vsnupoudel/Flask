5935fc3a7fb63a3310205d014530e08aa644d35f
vsnupoudel <replytobishnu@gmail.com>
2021-06-28 06:22:06 +0000

    to the final branch from tutors codebase

--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 from app import app, db
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for,flash, get_flashed_messages
 from models import Task
 from datetime import datetime
 import forms
@@ -17,8 +17,27 @@ def add():
         t = Task(title=form.title.data, date=datetime.utcnow())
         db.session.add(t)
         db.session.commit()
+        flash('Task added to the database')
         return redirect(url_for('index'))
        #return render_template('about.html', form= form,
        # title= form.title.data)
 
     return render_template('./add.html', form= form)
+
+
+
+@app.route('/edit/<int:task_id>', methods =['GET','POST'])
+def edit(task_id):
+    task = Task.query.get(task_id)
+    form = forms.AddTaskForm()
+    if task:
+        if form.validate_on_submit():
+            task.title = form.title.data
+            task.date = datetime.utcnow()
+            db.session.commit()
+            flash('Task has been updated')
+            return redirect(url_for('index'))
+        form.title.data = task.title
+        return render_template('edit.html', form= form, 
+        task_id=task_id)
+    return redirect(url_for('index'))
